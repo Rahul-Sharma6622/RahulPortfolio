30b24fbebe7391cb3620e507a499722069fd728c
Rahul Sharma <sharmarahul38174@gmail.com>
2026-03-30 17:40:43 +0530

    Nabar resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
